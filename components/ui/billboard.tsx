2ed66d8e266ef6a1281c4a1abf3c0415ba7efa31
Synonymous64 <prajwalurkude007@gmail.com>
2023-07-11 23:14:56 +0530

    minor fixes

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -10,10 +10,10 @@ const Billboard: React.FC<BillboardProps> = ({
   console.log(data);
   return ( 
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+      <div style={{ backgroundImage: `url(${data?.imageUrl ? data.imageUrl: 'https://cdn.pixabay.com/photo/2019/08/28/09/51/reyban-4436327_1280.jpg'})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-            {data.label}
+            {/* {data?.label ? data.label : 'Explore The Special Collection!'} */}
           </div>
         </div>
       </div>
